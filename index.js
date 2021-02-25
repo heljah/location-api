6ecc4ff5468dd05a386aec7b1909d4648f9e1706
HeljaH <>
2021-02-25 11:10:44 +0200

    added routing

--- index.js
+++ index.js
@@ -4,7 +4,15 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!');
+});
+
+app.get('/api/locations', (req, res) => {
+  res.send('fetching all locations');
+});
+
+app.post('/', function (req, res) {
+    res.send('Got a POST request');
 });
 
 // Tämä luo selaimeen polun, jolla pääsen public-kansiooni!
